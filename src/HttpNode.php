b46a3a5f8a8a82a4a56d7d737dc81c9e27d7fbf4
Yee Jia Wei <yeejiawei0811@gmail.com>
2022-04-01 00:02:32 +0800

    Create HttpNode.php
    Update HttpNode.php

---
+++ src/HttpNode.php
@@ -0,0 +1,124 @@
+<?php
+
+
+namespace Yeejiawei\TiktokShopApi;
+
+
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\RequestOptions;
+
+trait HttpNode
+{
+    protected string $api_path = '/api';
+
+    private function getClient()
+    {
+        return new GuzzleClient(['base_uri' => $this->client->apiHost]);
+    }
+
+    public function getParameters($uri, array $params)
+    {
+        $params = [
+            'app_key' => $this->client->appKey,
+            'access_token' => $this->client->accessToken,
+            'shop_id' => $this->client->shopId,
+            'timestamp' => time(),
+        ];
+
+        $params['sign'] = $this->generateSignature($uri, $params);
+
+
+        return $params;
+    }
+
+    private function getPath($uri): string
+    {
+        return $this->api_path . $this->getNodeEndpoint() . $uri;
+    }
+
+    protected function get($uri, array $params = [])
+    {
+        $params = array_merge($params, [
+            'app_key' => $this->client->appKey,
+            'access_token' => $this->client->accessToken,
+            'shop_id' => $this->client->shopId,
+            'timestamp' => time(),
+        ]);
+        $params = array_filter($params);
+
+        $params['sign'] = $this->generateSignature($uri, $params);
+
+        $response = $this->getClient()->request('GET', $this->getPath($uri), [
+            RequestOptions::QUERY => http_build_query($params),
+        ]);
+
+        $response = new Response($response, $params);
+
+        return $response->object();
+    }
+
+    protected function post($uri, array $params = [])
+    {
+        $params = $this->getParameters($uri, $params);
+
+        $response = $this->getClient()->request('POST', $this->getPath($uri), $params);
+
+        $response = new Response($response, $params);
+
+        return $response->object();
+    }
+
+    protected function put($uri, array $params = [])
+    {
+        $response = $this->getClient()->request('PUT', $this->getPath($uri), [
+            RequestOptions::QUERY => http_build_query($this->getParameters($uri, $params)),
+            RequestOptions::JSON => $params,
+        ]);
+
+        $response = new Response($response, $params);
+
+        return $response->object();
+    }
+
+    protected function delete($uri, array $params = [])
+    {
+//        $this->post($this->api_path . $this->getNodeEndpoint() . $uri);
+
+        return 'test';
+    }
+
+    private function generateSignature($uri, $params)
+    {
+        // secret
+        $secret = $this->client->appSecret;
+//                print_r(json_encode($params)); die();
+
+        // merge timestamp
+        $params = array_diff_key($params, ['sign' => '', 'access_token' => '']);
+
+        // Sort parameters
+        ksort($params);
+
+        $sign_string = $secret . $this->getPath($uri) . $this->buildParameterString($params) . $secret;
+
+//        print_r($sign_string); die();
+        return hash_hmac('sha256', $sign_string, $secret);
+    }
+
+    private function buildParameterString($params)
+    {
+        $string = '';
+
+        foreach ($params as $key => $value) {
+            if (is_array($value)) {
+//                print_r($value);die();
+//                $string .= $this->buildParameterString($value);
+//                print_r($string); die();
+            } else {
+                $string .= $key . $value;
+            }
+        }
+
+        return $string;
+    }
+}
