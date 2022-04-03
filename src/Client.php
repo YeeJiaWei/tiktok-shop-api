e477e58fd6aa5f7f993327b837616d88383b26c5
Yee Jia Wei <yeejiawei0811@gmail.com>
2022-04-03 09:15:39 +0800

    Update Client.php

--- src/Client.php
+++ src/Client.php
@@ -7,6 +7,13 @@ namespace Yeejiawei\TiktokShopApi;
 /**
  * Class Client
  * @package Yeejiawei\TiktokShopApi
+ *
+ * @method \Yeejiawei\TiktokShopApi\Nodes\Shop shop()
+ * @method \Yeejiawei\TiktokShopApi\Nodes\Product product()
+ * @method \Yeejiawei\TiktokShopApi\Nodes\Order order()
+ * @method \Yeejiawei\TiktokShopApi\Nodes\Logistics logistics()
+ * @method \Yeejiawei\TiktokShopApi\Nodes\Finance finance()
+ * @method \Yeejiawei\TiktokShopApi\Nodes\ReverseOrder reverseOrder()
  */
 class Client
 {
@@ -22,12 +29,21 @@ class Client
 
     public string $accessToken;
 
-    public function __construct(array $config)
+    public function __construct(array $config = [])
     {
         $this->appKey = $config['app_key'] ?? '';
         $this->appSecret = $config['app_secret'] ?? '';
         $this->shopId = $config['shop_id'] ?? '';
         $this->accessToken = $config['access_token'] ?? '';
+    }
+
+    public function __call($name, $arguments)
+    {
+        $className = 'Yeejiawei\\TiktokShopApi\\Nodes\\' . ucfirst($name);
+        if (!class_exists($className)) {
+            throw new \Exception("Class {$className} not found");
+        }
+        return new $className($this);
     }
 
 }
